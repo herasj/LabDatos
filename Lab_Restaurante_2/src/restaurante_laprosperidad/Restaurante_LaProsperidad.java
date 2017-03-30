ff1313e4c7cafe4a42d51215b21ed778292a0427
herasj <herasj@uninorte.edu.co>
2017-03-30 11:39:48 -0500

    Add files via upload
    Lab Datos

---
+++ Lab_Restaurante_2/src/restaurante_laprosperidad/Restaurante_LaProsperidad.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante_laprosperidad;
+
+/**
+ *
+ * @author cpu
+ */
+public class Restaurante_LaProsperidad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Main principal = new Main();
+       principal.setVisible(true);
+        
+        
+    }
+    
+}
